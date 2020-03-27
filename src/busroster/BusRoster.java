7ddb4923660d902366f8768c4f5c7bbc62eaf4d9
jfirpo <furedi.k@gmail.com>
2020-03-27 22:05:57 +0100

    Update BusRoster.java

--- src/busroster/BusRoster.java
+++ src/busroster/BusRoster.java
@@ -14,7 +14,7 @@ public class BusRoster {
         //inputDate = setInputDate();                                           // a kerdeses datum bekerese
         //teszteleshez bemeno adatok fixen. sofor es celdatum
         driver = myDataBase.getDriver("3210");                                  // tesztsofőr - kiválasztás direktben
-        inputDate = new GregorianCalendar(2020,1,2);                            // tesztdátum kiválasztás direktben
+        inputDate = new GregorianCalendar(2020,1,2);                            // tesztdátum kiválasztás direktben  ,.,.
         SwapRepository swaps = new SwapRepository(driver, inputDate, myDataBase);
 
         if (swaps.doesItNeedASwap()){
